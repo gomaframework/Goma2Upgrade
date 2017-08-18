c5b0c97cae22e961fd6f7b6bc52e680495c7c5bc
Daniel Gruber <daniel@ibpg.eu>
2017-08-18 15:00:29 +0200

    - Switch from aliasing to ClassMap to help IDEs understand new structure

---
+++ src/classes/ExceptionManager.php
@@ -0,0 +1,16 @@
+<?php
+
+defined("IN_GOMA") OR die();
+
+/**
+ * Describe your file.
+ *
+ * @package    goma/Goma2Upgrade
+ * @link    http://goma-cms.org
+ * @license LGPL http://www.gnu.org/copyleft/lesser.html see 'license.txt'
+ * @author    Goma-Team
+ */
+class ExceptionManager extends \Goma\Error\ExceptionManager
+{
+
+}
---
+++ src/classes/GomaException.php
@@ -0,0 +1,16 @@
+<?php
+
+defined("IN_GOMA") OR die();
+
+/**
+ * Goma-Exception alias.
+ *
+ * @package    goma/Goma2Upgrade
+ * @link    http://goma-cms.org
+ * @license LGPL http://www.gnu.org/copyleft/lesser.html see 'license.txt'
+ * @author    Goma-Team
+ */
+class GomaException extends \Goma\Error\GomaException
+{
+
+}
